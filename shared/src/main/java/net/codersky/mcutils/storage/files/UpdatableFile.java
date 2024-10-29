b54121ea5800b7497a6210b2906cd287ba587def
xDec0de_ <xdec0deyt@gmail.com>
2024-10-30 00:01:42 +0100

    UpdatableFile: Added more default methods

--- shared/src/main/java/net/codersky/mcutils/storage/files/UpdatableFile.java
+++ shared/src/main/java/net/codersky/mcutils/storage/files/UpdatableFile.java
@@ -1,20 +1,60 @@
 package net.codersky.mcutils.storage.files;
 
-import net.codersky.mcutils.Reloadable;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public interface UpdatableFile extends Reloadable {
+/**
+ * Interface designed for files that can be updated,
+ * optionally ignoring a list of paths.
+ *
+ * @since MCUtils 1.0.0
+ *
+ * @see #update(List)
+ * @see #update(String...)
+ * @see #update()
+ *
+ * @author xDec0de_
+ */
+public interface UpdatableFile {
 
 	/**
 	 * Updates any files handled by this {@link UpdatableFile}.
 	 *
 	 * @param ignored A list of paths that shall be
-	 * ignored when updating files.
+	 * ignored when updating files, {@code null} or an empty
+	 * list indicates no paths are ignored.
 	 *
-	 * @return True on success, false on failure.
+	 * @return {@code true} on success, {@code false} on failure.
 	 *
 	 * @since MCUtils 1.0.0
 	 */
-	boolean update(List<String> ignored);
+	boolean update(@Nullable List<String> ignored);
+
+	/**
+	 * Updates any files handled by this {@link UpdatableFile}.
+	 *
+	 * @param ignored An array of paths that shall be
+	 * ignored when updating files, {@code null} or an empty
+	 * array indicates no paths are ignored.
+	 *
+	 * @return {@code true} on success, {@code false} on failure.
+	 *
+	 * @since MCUtils 1.0.0
+	 */
+	default boolean update(@Nullable String... ignored) {
+		return ignored == null ? update() : update(List.of(ignored));
+	}
+
+	/**
+	 * Updates any files handled by this {@link UpdatableFile}
+	 * without ignoring any path.
+	 *
+	 * @return {@code true} on success, {@code false} on failure.
+	 *
+	 * @since MCUtils 1.0.0
+	 */
+	default boolean update() {
+		return update(List.of());
+	}
 }
