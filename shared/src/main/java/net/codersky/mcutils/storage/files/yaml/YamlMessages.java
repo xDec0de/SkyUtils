b9c2b53e43ef49c453b85589b4812bf7797c65b2
xDec0de_ <xdec0deyt@gmail.com>
2024-10-29 22:11:40 +0100

    YamlMessages: Implement MessagesFile

--- shared/src/main/java/net/codersky/mcutils/storage/files/yaml/YamlMessages.java
+++ shared/src/main/java/net/codersky/mcutils/storage/files/yaml/YamlMessages.java
@@ -1,79 +1,18 @@
 package net.codersky.mcutils.storage.files.yaml;
 
-import net.codersky.mcutils.crossplatform.MessageReceiver;
-import net.codersky.mcutils.java.strings.MCStrings;
-import net.codersky.mcutils.java.strings.Replacer;
+import net.codersky.mcutils.storage.files.MessagesFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-
-public class YamlMessages extends YamlFile {
-
-	protected final HashMap<String, Object> keys = new HashMap<>();
+public class YamlMessages extends YamlFile implements MessagesFile {
 
 	public YamlMessages(String path) {
 		super(path);
 	}
 
-	/*
-	 * Raw message getters
-	 */
-
 	@Nullable
+	@Override
 	public String getRawMessage(@NotNull String path) {
-		final Object obj = keys.get(path);
-		return obj instanceof String str ? str : null;
-	}
-
-	@Nullable
-	public String getRawMessage(@NotNull String path, @NotNull Replacer replacer) {
-		final String str = getMessage(path);
-		return str == null ? null : replacer.replaceAt(str);
-	}
-
-	@Nullable
-	public String getRawMessage(@NotNull String path, @NotNull Object... replacements) {
-		return getMessage(path, new Replacer(replacements));
-	}
-
-	/*
-	 * Message getters
-	 */
-
-	@Nullable
-	public String getMessage(@NotNull String path) {
-		final String raw = getRawMessage(path);
-		return raw == null ? null : MCStrings.applyColor(raw);
-	}
-
-	@Nullable
-	public String getMessage(@NotNull String path, @NotNull Replacer replacer) {
-		final String str = getMessage(path);
-		return str == null ? null : replacer.replaceAt(str);
-	}
-
-	@Nullable
-	public String getMessage(@NotNull String path, @NotNull Object... replacements) {
-		return getMessage(path, new Replacer(replacements));
-	}
-
-	/*
-	 * Message senders
-	 */
-
-	public boolean sendMessage(@NotNull MessageReceiver receiver, @NotNull String path) {
-		final String raw = getRawMessage(path);
-		return raw == null || MCStrings.sendMessage(receiver, raw);
-	}
-
-	public boolean sendMessage(@NotNull MessageReceiver receiver, @NotNull String path, @NotNull Replacer replacer) {
-		final String raw = getRawMessage(path, replacer);
-		return raw == null || MCStrings.sendMessage(receiver, raw);
-	}
-
-	public boolean sendMessage(@NotNull MessageReceiver receiver, @NotNull String path, @NotNull Object... replacements) {
-		final String raw = getRawMessage(path, replacements);
-		return raw == null || MCStrings.sendMessage(receiver, raw);
+		return get(path, String.class);
 	}
 }
