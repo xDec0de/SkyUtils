8782b40b9120d29df726d4f44bbc4defa0b17196
xDec0de_ <xdec0deyt@gmail.com>
2024-10-29 19:53:00 +0100

    YamlManager: Added basic update method

--- shared/src/main/java/net/codersky/mcutils/storage/files/yaml/YamlManager.java
+++ shared/src/main/java/net/codersky/mcutils/storage/files/yaml/YamlManager.java
@@ -1,6 +1,7 @@
 package net.codersky.mcutils.storage.files.yaml;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
@@ -9,21 +10,30 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 class YamlManager {
 
 	private final File file;
 	private final Yaml yaml;
 
-	YamlManager(String path) {
+	YamlManager(@NotNull String path) {
+		this.yaml = getNewYaml();
+		this.file = new File(path);
+	}
+
+	private Yaml getNewYaml() {
 		final DumperOptions dumperOptions = new DumperOptions();
 		dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.FLOW);
-		this.yaml = new Yaml(dumperOptions);
-		this.file = new File(path);
+		return new Yaml(dumperOptions);
 	}
 
 	@NotNull
@@ -50,5 +60,12 @@ class YamlManager {
 		} catch (IOException e) {
 			return false;
 		}
+	}
+
+	public void update(@NotNull InputStream source, @NotNull HashMap<String, Object> keys) {
+		final HashMap<String, Object> updMap = getNewYaml().load(source);
+		for (Map.Entry<String, Object> entry : updMap.entrySet())
+			if (!keys.containsKey(entry.getKey()))
+				keys.put(entry.getKey(), entry.getValue());
 	}
 }
