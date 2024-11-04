8d8087c31fe500c831d22a4c64b71adf265ceef8
xDec0de_ <xdec0deyt@gmail.com>
2024-11-04 21:16:51 +0100

    SoundTargetPattern: Volume & pitch support

--- shared/src/main/java/net/codersky/mcutils/java/strings/pattern/target/SoundTargetPattern.java
+++ shared/src/main/java/net/codersky/mcutils/java/strings/pattern/target/SoundTargetPattern.java
@@ -2,6 +2,7 @@ package net.codersky.mcutils.java.strings.pattern.target;
 
 import net.codersky.mcutils.crossplatform.MessageReceiver;
 import net.codersky.mcutils.crossplatform.player.MCPlayer;
+import net.codersky.mcutils.java.math.MCNumbers;
 import net.codersky.mcutils.java.strings.MCStrings;
 import net.codersky.mcutils.java.strings.pattern.TargetPattern;
 import org.jetbrains.annotations.NotNull;
@@ -12,11 +13,17 @@ public class SoundTargetPattern implements TargetPattern {
 	@Override
 	public String process(@NotNull MessageReceiver target, @NotNull String string, boolean applyEventPatterns) {
 		return MCStrings.match(string, "<sound:", "/>", message -> {
-			if (!(target instanceof MCPlayer))
+			if (!(target instanceof final MCPlayer player))
 				return;
-			final MCPlayer player = (MCPlayer) target;
-			final String sound = message.indexOf(":") > 1 ? message : "minecraft:" + message;
-			player.playSound(sound);
+			final String[] soundInfo = message.split(";");
+			float volume = 1;
+			float pitch = 1;
+			if (soundInfo.length >= 1)
+				volume = MCNumbers.asNumber(soundInfo[0], 1f);
+			if (soundInfo.length >= 2)
+				pitch = MCNumbers.asNumber(soundInfo[1], 1f);
+			final String sound = soundInfo[soundInfo.length - 1];
+			player.playSound(sound.indexOf(":") > 1 ? sound : "minecraft:" + sound, volume, pitch);
 		});
 	}
 }
