660a6df6f2a58fc8ddf0134ec78c8fbceed4e105
xDec0de_ <xdec0deyt@gmail.com>
2024-11-05 03:00:13 +0100

    Update MCUtilsSpigot documentation
    MCUtils is also designed to be used as a plugin. This documentation implies that the class exists only for testing, but this is no longer the case

--- platforms/spigot/src/main/java/net/codersky/mcutils/spigot/MCUtilsSpigot.java
+++ platforms/spigot/src/main/java/net/codersky/mcutils/spigot/MCUtilsSpigot.java
@@ -5,9 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 /**
  * The MCUtils class, this class is not intended to be
  * used as it provides nothing, just an empty {@link #onEnable()}
- * method, this class is here to make testing easier so MCUtils can
- * run as a plugin instead of a shaded library (Used to test MCUtils
- * changes locally as an alternative to maven local).
+ * method, this class is here so MCUtils can be used as a Spigot plugin.
  * 
  * @author xDec0de_
  *
@@ -15,14 +13,6 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class MCUtilsSpigot extends JavaPlugin {
 
-	/**
-	 * Codacy was complaining about this method not
-	 * being documented while being empty, so yeah,
-	 * here is some documentation to it, this method
-	 * is here so MCUtils can be used as a plugin for
-	 * testing changes to it without pushing to the
-	 * maven repository.
-	 */
 	@Override
 	public void onEnable() {
 		// This method is intentionally empty, Codacy, please, stop complaining :(
