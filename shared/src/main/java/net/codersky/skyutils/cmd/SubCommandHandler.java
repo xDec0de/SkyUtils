6679a4f1f1753e54add5bea3f796b33d9955f06f
xDec0de_ <xdec0deyt@gmail.com>
2024-12-06 10:47:29 +0100

    SubCommandHandler: Filter tab complete
    This allows to return a null list on tab complete without any issues, as well as filtering tab completions that don't match with the current argument being typed

--- shared/src/main/java/net/codersky/skyutils/cmd/SubCommandHandler.java
+++ shared/src/main/java/net/codersky/skyutils/cmd/SubCommandHandler.java
@@ -1,8 +1,11 @@
 package net.codersky.skyutils.cmd;
 
+import net.codersky.skyutils.java.SkyCollections;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -12,16 +15,6 @@ public class SubCommandHandler<P, S extends MCCommandSender> {
 
 	private final HashSet<MCCommand<P, S>> subCommands = new HashSet<>();
 
-	@NotNull
-	private String[] removeFirstArgument(@NotNull String[] args) {
-		if (args.length == 1)
-			return new String[0];
-		final String[] newArgs = new String[args.length - 1];
-		for (int i = 1; i < args.length; i++)
-			newArgs[i - 1] = args[1];
-		return newArgs;
-	}
-
 	private <T> T onUsedCommand(@NotNull MCCommand<P, S> mainCmd, @NotNull S sender, @NotNull String[] args,
 	                            @NotNull BiFunction<MCCommand<P, S>, String[], T> action, @NotNull T def, boolean message) {
 		if (!mainCmd.hasAccess(sender, message))
@@ -29,29 +22,69 @@ public class SubCommandHandler<P, S extends MCCommandSender> {
 		if (args.length == 0)
 			return action.apply(mainCmd, args);
 		for (MCCommand<P, S> subCommand : subCommands)
-			if (subCommand.getName().equalsIgnoreCase(args[0]))
-				return subCommand.hasAccess(sender, message) ? action.apply(subCommand, removeFirstArgument(args)) : def;
+			if (subCommand.matches(args[0]))
+				return onUsedCommand(subCommand, sender, Arrays.copyOfRange(args, 1, args.length), action, def, message);
 		return action.apply(mainCmd, args);
 	}
+
+	/*
+	 - Command execution
+	 */
 
 	public boolean onCommand(@NotNull MCCommand<P, S> mainCmd, @NotNull S sender, @NotNull String[] args) {
 		return onUsedCommand(mainCmd, sender, args, (cmd, newArgs) -> cmd.onCommand(sender, newArgs), true, true);
 	}
 
+	/*
+	 - Tab complete
+	 */
+
 	public List<String> onTab(@NotNull MCCommand<P, S> mainCommand, @NotNull S sender, @NotNull String[] args) {
 		return onUsedCommand(mainCommand, sender, args, (cmd, newArgs) -> {
 			if (subCommands.isEmpty() || args.length > 1)
-				return cmd.onTab(sender, newArgs);
-			final List<String> cmdTabs = cmd.onTab(sender, newArgs);
+				return prepareSuggestions(cmd.onTab(sender, newArgs), newArgs);
+			List<String> cmdTabs = cmd.onTab(sender, newArgs);
+			if (cmdTabs == null)
+				cmdTabs = new ArrayList<>(0);
 			final List<String> tabs = new ArrayList<>(subCommands.size() + cmdTabs.size());
 			tabs.addAll(cmdTabs);
 			for (MCCommand<P, S> subCmd : subCommands) {
 				tabs.add(subCmd.getName());
 				tabs.addAll(subCmd.getAliases());
 			}
-			return tabs;
+			return prepareSuggestions(tabs, newArgs);
 		}, List.of(), false);
 	}
+
+	/**
+	 * Prepares a list of {@code suggestions} to be used by {@link #onTab(MCCommand, MCCommandSender, String[])}.
+	 * By default, this method will:
+	 * <ul>
+	 * <li>Return an {@link List#of() empty list} if {@code suggestions} is {@code null}.</li>
+	 * <li>Filter {@code suggestions} based on the last argument from {@code args}, case insensitive,
+	 * meaning that {@code suggestions} that don't start with the last argument won't be included</li>
+	 * </ul>
+	 *
+	 * @param suggestions The list of suggestions to prepare.
+	 * @param args The command arguments to use for filtering.
+	 *
+	 * @return A {@link NotNull} list of suggestions to be used on the
+	 * {@link #onTab(MCCommand, MCCommandSender, String[])} method.
+	 *
+	 * @since SkyUtils 1.0.0
+	 */
+	protected List<String> prepareSuggestions(@Nullable List<String> suggestions, @NotNull String[] args) {
+		if (suggestions == null || suggestions.isEmpty())
+			return List.of();
+		if (args.length == 0)
+			return suggestions;
+		final String lastArg = args[args.length - 1].toLowerCase();
+		return SkyCollections.clone(suggestions, tab -> tab.startsWith(lastArg));
+	}
+
+	/*
+	 - SubCommand injection
+	 */
 
 	@SafeVarargs
 	public final void inject(@NotNull MCCommand<P, S>... commands) {
