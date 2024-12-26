7825fc8185effbb7a591db293b95f850cbaf07ff
xDec0de_ <xdec0deyt@gmail.com>
2024-12-26 20:23:05 +0100

    Proper offline player interface
    Offline players shouldn't be an adaptation of a possibly online player, doing that only favors lazy coding that doesn't check if players are online. This way we must actually obtain an online player instance to use methods that are for online players only... Pretty simple

--- shared/src/main/java/net/codersky/skyutils/crossplatform/player/SkyPlayer.java
+++ shared/src/main/java/net/codersky/skyutils/crossplatform/player/SkyPlayer.java
@@ -1,21 +1,157 @@
 package net.codersky.skyutils.crossplatform.player;
 
-import net.codersky.skyutils.java.strings.Replacement;
+import net.codersky.skyutils.cmd.SkyCommand;
+import net.codersky.skyutils.crossplatform.MessageReceiver;
+import net.codersky.skyutils.java.strings.Replacer;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.key.KeyPattern;
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Cross-platform interface used to represent an <b>online</b> player.
  * This interface allows developers to use common methods
  * on all platforms without needing to actually use a
  * platform specific player type.
- * <p>
- * This interface extends the {@link Replacement} interface. Overriding
- * {@link Replacement#asReplacement()} with a call to {@link #getName()}
- * by default.
  *
  * @since SkyUtils 1.0.0
  *
  * @author xDec0de_
  */
-public interface SkyPlayer extends OfflineSkyPlayer {
+public interface SkyPlayer extends OfflineSkyPlayer, MessageReceiver {
 
+	@NotNull
+	@Override
+	default String asReplacement() {
+		return getName();
+	}
+
+	/*
+	 - Legacy actionbar messages (String)
+	 */
+
+	/**
+	 * Sends an ActionBar {@code message} to this {@link OfflineSkyPlayer player}.
+	 *
+	 * @param message The message to send to this {@link OfflineSkyPlayer player}.
+	 *
+	 * @return Always {@code true} to make it easier to create {@link SkyCommand SkyCommands}.
+	 *
+	 * @throws NullPointerException if {@code message} is {@code null}
+	 *
+	 * @since SkyUtils 1.0.0
+	 */
+	boolean sendActionBar(@NotNull String message);
+
+	/**
+	 * Sends an ActionBar {@code message} to this {@link OfflineSkyPlayer player}, applying {@code replacer}
+	 * to {@code message} before sending it.
+	 *
+	 * @param message The ActionBar message to send to this {@link OfflineSkyPlayer player}.
+	 * @param replacer The {@link Replacer} to apply to the {@code message} before sending it.
+	 *
+	 * @return Always {@code true} to make it easier to create {@link SkyCommand SkyCommands}.
+	 *
+	 * @throws NullPointerException if any parameter is {@code null}
+	 *
+	 * @since SkyUtils 1.0.0
+	 */
+	default boolean sendActionBar(@NotNull String message, @NotNull Replacer replacer) {
+		return !canReceive(message) || sendActionBar(replacer.replaceAt(message));
+	}
+
+	/**
+	 * Sends an ActionBar {@code message} to this {@link OfflineSkyPlayer player}, applying a {@link Replacer} made
+	 * with the specified {@code replacements} to {@code message} before sending it.
+	 *
+	 * @param message The message to send to this {@link OfflineSkyPlayer player}.
+	 * @param replacements The replacements used to build a {@link Replacer} that will then be
+	 * applied to the {@code message} before sending it. The amount of replacements must be even
+	 * as specified on the {@link Replacer} {@link Replacer#Replacer constructor}.
+	 *
+	 * @return Always {@code true} to make it easier to create {@link SkyCommand SkyCommands}.
+	 *
+	 * @throws NullPointerException if any parameter is {@code null}
+	 *
+	 * @since SkyUtils 1.0.0
+	 */
+	default boolean sendActionBar(@NotNull String message, @NotNull Object... replacements) {
+		return !canReceive(message) || sendActionBar(message, new Replacer(replacements));
+	}
+
+	/*
+	 - Actionbar Adventure messages
+	 */
+
+	/**
+	 * Sends an ActionBar {@link Component} {@code message} to this {@link OfflineSkyPlayer player}.
+	 *
+	 * @param message The Adventure {@link Component} to send to this {@link OfflineSkyPlayer player}.
+	 *
+	 * @return Always {@code true} to make it easier to create {@link SkyCommand SkyCommands}.
+	 *
+	 * @throws NullPointerException if {@code message} is {@code null}
+	 *
+	 * @since SkyUtils 1.0.0
+	 */
+	boolean sendActionBar(@NotNull Component message);
+
+	/**
+	 * Sends an ActionBar {@link Component} {@code message} to this {@link OfflineSkyPlayer player},
+	 * applying {@code replacer} to {@code message} before sending it.
+	 *
+	 * @param message The Adventure {@link Component} to send to this {@link OfflineSkyPlayer player}.
+	 * @param replacer The {@link Replacer} to apply to the {@code message} before sending it.
+	 *
+	 * @return Always {@code true} to make it easier to create {@link SkyCommand SkyCommands}.
+	 *
+	 * @throws NullPointerException if any parameter is {@code null}
+	 *
+	 * @since SkyUtils 1.0.0
+	 */
+	default boolean sendActionBar(@NotNull Component message, @NotNull Replacer replacer) {
+		return !canReceive(message) || sendActionBar(replacer.replaceAt(message));
+	}
+
+	/**
+	 * Sends an ActionBar {@link Component} {@code message} to this {@link OfflineSkyPlayer player}, applying
+	 * a {@link Replacer} made with the specified {@code replacements} to {@code message} before sending it.
+	 *
+	 * @param message The Adventure {@link Component} to send to this {@link OfflineSkyPlayer player}.
+	 * @param replacements The replacements used to build a {@link Replacer} that will then be
+	 * applied to the {@code message} before sending it. The amount of replacements must be even
+	 * as specified on the {@link Replacer} {@link Replacer#Replacer constructor}.
+	 *
+	 * @return Always {@code true} to make it easier to create {@link SkyCommand SkyCommands}.
+	 *
+	 * @throws NullPointerException if any parameter is {@code null}
+	 *
+	 * @since SkyUtils 1.0.0
+	 */
+	default boolean sendActionBar(@NotNull Component message, @NotNull Object... replacements) {
+		return !canReceive(message) || sendActionBar(message, new Replacer(replacements));
+	}
+
+	/*
+	 - Sound
+	 */
+
+	boolean playSound(@NotNull Sound sound);
+
+	default boolean playSound(@KeyPattern @NotNull String key, @NotNull Sound.Source source, float volume, float pitch) {
+		return playSound(Sound.sound(Key.key(key), source, volume, pitch));
+	}
+
+	default boolean playSound(@KeyPattern @NotNull String key, float volume, float pitch) {
+		return playSound(key, Sound.Source.AMBIENT, volume, pitch);
+	}
+
+	default boolean playSound(@KeyPattern @NotNull String key, float volume) {
+		return playSound(key, volume, 1.0f);
+	}
+
+	default boolean playSound(@KeyPattern @NotNull String key) {
+		return playSound(key, 1.0f);
+	}
 }
