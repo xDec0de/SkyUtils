f6629ae861cfe142989b68397b988eebad156e6a
xDec0de_ <xdec0deyt@gmail.com>
2024-12-26 20:40:12 +0100

    VelocityTaskScheduler: Store Scheduler, not ProxyServer

--- platforms/velocity/src/main/java/net/codersky/skyutils/velocity/time/VelocityTaskScheduler.java
+++ platforms/velocity/src/main/java/net/codersky/skyutils/velocity/time/VelocityTaskScheduler.java
@@ -1,6 +1,7 @@
 package net.codersky.skyutils.velocity.time;
 
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.scheduler.Scheduler;
 import net.codersky.skyutils.time.TaskScheduler;
 import org.jetbrains.annotations.NotNull;
 
@@ -8,42 +9,48 @@ import java.util.concurrent.TimeUnit;
 
 public class VelocityTaskScheduler implements TaskScheduler {
 
-	private final ProxyServer server;
+	private final Scheduler scheduler;
 	private final Object plugin;
 
 	public VelocityTaskScheduler(@NotNull ProxyServer server, @NotNull Object plugin) {
-		this.server = server;
+		this.scheduler = server.getScheduler();
 		this.plugin = plugin;
 	}
 
+	@NotNull
 	@Override
-	public @NotNull VelocityTask runSync(@NotNull Runnable task) {
+	public VelocityTask runSync(@NotNull Runnable task) {
 		return runAsync(task);
 	}
 
+	@NotNull
 	@Override
-	public @NotNull VelocityTask delaySync(@NotNull Runnable task, @NotNull TimeUnit unit, int delay) {
+	public VelocityTask delaySync(@NotNull Runnable task, @NotNull TimeUnit unit, int delay) {
 		return delayAsync(task, unit, delay);
 	}
 
+	@NotNull
 	@Override
-	public @NotNull VelocityTask repeatSync(@NotNull Runnable task, @NotNull TimeUnit unit, int delay, int repeat) {
+	public VelocityTask repeatSync(@NotNull Runnable task, @NotNull TimeUnit unit, int delay, int repeat) {
 		return repeatAsync(task, unit, delay, repeat);
 	}
 
+	@NotNull
 	@Override
-	public @NotNull VelocityTask runAsync(@NotNull Runnable task) {
-		return new VelocityTask(server.getScheduler().buildTask(plugin, task).schedule());
+	public VelocityTask runAsync(@NotNull Runnable task) {
+		return new VelocityTask(scheduler.buildTask(plugin, task).schedule());
 	}
 
+	@NotNull
 	@Override
-	public @NotNull VelocityTask delayAsync(@NotNull Runnable task, @NotNull TimeUnit unit, int delay) {
-		return new VelocityTask(server.getScheduler().buildTask(plugin, task).delay(delay, unit).schedule());
+	public VelocityTask delayAsync(@NotNull Runnable task, @NotNull TimeUnit unit, int delay) {
+		return new VelocityTask(scheduler.buildTask(plugin, task).delay(delay, unit).schedule());
 	}
 
+	@NotNull
 	@Override
-	public @NotNull VelocityTask repeatAsync(@NotNull Runnable task, @NotNull TimeUnit unit, int delay, int repeat) {
-		return new VelocityTask(server.getScheduler().buildTask(plugin, task)
+	public VelocityTask repeatAsync(@NotNull Runnable task, @NotNull TimeUnit unit, int delay, int repeat) {
+		return new VelocityTask(scheduler.buildTask(plugin, task)
 				.delay(delay, unit)
 				.repeat(repeat, unit)
 				.schedule());
