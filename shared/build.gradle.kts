b71c5318b7bdbd587b47b782380ce4669aaaf815
xDec0de_ <xdec0deyt@gmail.com>
2025-01-27 10:24:16 +0100

    Gradle setup to shade JSky into SkyUtils
    The Codersky repository is currently down due to some issues with the hosting company. However, JSky will be the core of SkyUtils, so I decided to get some work done at least with gradle. The gradle build logic needs a refresh though, as it kind of "works", but not as I would like. Also publishing has some issues with the sources of dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
 	skyutils.`library-conventions`
+	skyutils.`shadow-conventions`
 }
 
 dependencies {
+	api(libs.jsky)
 	compileOnly(libs.jetbrains.annotations)
 	compileOnly(libs.adventure.serializer.legacy)
 	compileOnly(libs.snakeyaml)
