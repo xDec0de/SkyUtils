9b103749329c36f8293771f8d9ec2c6069a7e964
xDec0de_ <xdec0deyt@gmail.com>
2025-03-26 22:16:11 +0100

    Gradle: Remove JSky until Codersky repo is online

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,7 +4,8 @@ plugins {
 }
 
 dependencies {
-	api(libs.jsky)
+	// TODO: Implement JSky once the Codersky repository is back online.
+	// api(libs.jsky)
 	compileOnly(libs.jetbrains.annotations)
 	compileOnly(libs.adventure.serializer.legacy)
 	compileOnly(libs.snakeyaml)
