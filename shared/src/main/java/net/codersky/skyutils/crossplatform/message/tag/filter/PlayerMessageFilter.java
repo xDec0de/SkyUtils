a4e2657b884d2eab3613952fe990cf9d7c7bc0c0
xDec0de_ <xdec0deyt@gmail.com>
2025-05-07 23:17:23 +0200

    Added player message filter

---
+++ shared/src/main/java/net/codersky/skyutils/crossplatform/message/tag/filter/PlayerMessageFilter.java
@@ -0,0 +1,25 @@
+package net.codersky.skyutils.crossplatform.message.tag.filter;
+
+import net.codersky.skyutils.crossplatform.MessageReceiver;
+import net.codersky.skyutils.crossplatform.player.SkyPlayer;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerMessageFilter implements MessageFilter {
+
+	private final String[] aliases = {"player"};
+
+	@Override
+	public boolean filter(@NotNull MessageReceiver receiver) {
+		return receiver instanceof SkyPlayer;
+	}
+
+	@Override
+	public @NotNull String getKey() {
+		return "p";
+	}
+
+	@Override
+	public @NotNull String @NotNull [] getAliases() {
+		return aliases;
+	}
+}
