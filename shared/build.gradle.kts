5799d3ad5fe042edea6fd0d7d4c8d485e16d9fbf
xDec0de_ <xdec0deyt@gmail.com>
2025-05-09 19:52:44 +0200

    Shade dependencies on shared jar

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,8 +4,20 @@ plugins {
 }
 
 dependencies {
-	compileOnly(libs.jsky.base)
+	api(libs.jsky.base)
 	compileOnly(libs.jsky.yaml)
 	compileOnly(libs.jetbrains.annotations)
-	compileOnly(libs.adventure.serializer.legacy)
+	implementation(libs.adventure.api)
+	implementation(libs.adventure.serializer.legacy)
+	implementation(libs.adventure.serializer.gson)
+}
+
+tasks {
+	shadowJar {
+		relocate("net.kyori", "net.codersky.skyutils.shaded.kyori")
+		relocate("com.google", "net.codersky.skyutils.shaded.google")
+		exclude("org/jetbrains/**",
+			"org/intellij/**",)
+		minimize()
+	}
 }
