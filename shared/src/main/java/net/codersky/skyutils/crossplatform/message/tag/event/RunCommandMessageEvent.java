2e43dd3ee9e75d65a7a30912f9b118c5883abc11
xDec0de_ <xdec0deyt@gmail.com>
2025-05-28 02:33:18 +0200

    Added RunCommandMessageEvent

---
+++ shared/src/main/java/net/codersky/skyutils/crossplatform/message/tag/event/RunCommandMessageEvent.java
@@ -0,0 +1,31 @@
+package net.codersky.skyutils.crossplatform.message.tag.event;
+
+import net.codersky.skyutils.crossplatform.message.MessageType;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class RunCommandMessageEvent implements MessageEvent {
+
+	public static final RunCommandMessageEvent INSTANCE = new RunCommandMessageEvent();
+	private final String[] aliases = {"run_cmd", "run_command"};
+
+	private RunCommandMessageEvent() {}
+
+	@Override
+	public @NotNull Component apply(@NotNull MessageType type, @NotNull Component component, @NotNull String context) {
+		if (type != MessageType.CHAT)
+			return component;
+		return component.clickEvent(ClickEvent.runCommand(context));
+	}
+
+	@Override
+	public @NotNull String getKey() {
+		return "run";
+	}
+
+	@Override
+	public @NotNull String @NotNull [] getAliases() {
+		return aliases;
+	}
+}
