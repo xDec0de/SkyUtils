f6407fa96758ba50af2a6e1e7c550b6f715e415a
xDec0de_ <xdec0deyt@gmail.com>
2025-05-28 02:33:37 +0200

    Added SuggestCommandMessageEvent

---
+++ shared/src/main/java/net/codersky/skyutils/crossplatform/message/tag/event/SuggestCommandMessageEvent.java
@@ -0,0 +1,31 @@
+package net.codersky.skyutils.crossplatform.message.tag.event;
+
+import net.codersky.skyutils.crossplatform.message.MessageType;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class SuggestCommandMessageEvent implements MessageEvent {
+
+	public static final SuggestCommandMessageEvent INSTANCE = new SuggestCommandMessageEvent();
+	private final String[] aliases = {"suggest_cmd", "suggest_command"};
+
+	private SuggestCommandMessageEvent() {}
+
+	@Override
+	public @NotNull Component apply(@NotNull MessageType type, @NotNull Component component, @NotNull String context) {
+		if (type != MessageType.CHAT)
+			return component;
+		return component.clickEvent(ClickEvent.suggestCommand(context));
+	}
+
+	@Override
+	public @NotNull String getKey() {
+		return "suggest";
+	}
+
+	@Override
+	public @NotNull String @NotNull [] getAliases() {
+		return aliases;
+	}
+}
