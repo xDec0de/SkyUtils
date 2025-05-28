f8e75f119e1142ce4689e4e99941d5f914c52eb7
xDec0de_ <xdec0deyt@gmail.com>
2025-05-28 02:33:07 +0200

    Added OpenUrlMessageEvent

---
+++ shared/src/main/java/net/codersky/skyutils/crossplatform/message/tag/event/OpenUrlMessageEvent.java
@@ -0,0 +1,31 @@
+package net.codersky.skyutils.crossplatform.message.tag.event;
+
+import net.codersky.skyutils.crossplatform.message.MessageType;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class OpenUrlMessageEvent implements MessageEvent {
+
+	public static final OpenUrlMessageEvent INSTANCE = new OpenUrlMessageEvent();
+	private final String[] aliases = {"open_url"};
+
+	private OpenUrlMessageEvent() {}
+
+	@Override
+	public @NotNull Component apply(@NotNull MessageType type, @NotNull Component component, @NotNull String context) {
+		if (type != MessageType.CHAT)
+			return component;
+		return component.clickEvent(ClickEvent.openUrl(context));
+	}
+
+	@Override
+	public @NotNull String getKey() {
+		return "url";
+	}
+
+	@Override
+	public @NotNull String @NotNull [] getAliases() {
+		return aliases;
+	}
+}
